30a5594a13c8817ffb2b1a43249a99f0f1047b68
Taylor <ubermouse894@gmail.com>
2012-10-27 22:14:09 +1300

    I don

--- MonoGame/MonoGame.Framework.Content.Pipeline/Serialization/Compiler/ContentTypeWriterAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler
-{
-    [AttributeUsage(AttributeTargets.Class)]
-    public sealed class ContentTypeWriterAttribute : Attribute { }
-}
