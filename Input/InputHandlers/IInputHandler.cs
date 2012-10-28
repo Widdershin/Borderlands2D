9e36caaadc9250baa4cf14a0ffc6986ec82d5437
Taylor <ubermouse894@gmail.com>
2012-10-28 22:37:46 +1300

    Added Update method to Input Handlers

--- Input/InputHandlers/IInputHandler.cs
+++ Input/InputHandlers/IInputHandler.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Borderlands2D.Input.InputHandlers
 {
     interface IInputHandler
     {
         bool IsActive(Inputs input);
+
+        void Update(GameTime time);
     }
 }
--- Input/InputHandlers/KeyBoardAndMouse.cs
+++ Input/InputHandlers/KeyBoardAndMouse.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
 
 namespace Borderlands2D.Input.InputHandlers
 {
     class KeyBoardAndMouse : IInputHandler
     {
         public bool IsActive(Inputs input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime time)
         {
             throw new NotImplementedException();
         }
