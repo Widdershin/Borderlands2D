881d83e20f75d6ce8ad1c5f3b4d40c05b4116ae4
Taylor <ubermouse894@gmail.com>
2012-11-03 17:50:19 +1300

    Updated MonoGame revision

--- Borderlands2D.cs
+++ Borderlands2D.cs
@@ -1,15 +1,12 @@
 ﻿#region Using Statements
-using System;
-using System.Collections.Generic;
+
 using System.IO;
 using Borderlands2D.Input;
 using Borderlands2D.Input.InputHandlers;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.GamerServices;
+
 #endregion
 
 namespace Borderlands2D
