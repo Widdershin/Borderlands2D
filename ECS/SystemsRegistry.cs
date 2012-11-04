77479381c5a4cdcc40332034aa5a73a5007fe460
Taylor <ubermouse894@gmail.com>
2012-11-04 20:12:40 +1300

    Added SystemsRegistry

---
+++ ECS/SystemsRegistry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Borderlands2D.ECS
+{
+    sealed public class SystemsRegistry
+    {
+        private static readonly Dictionary<Type[], List<EntitySystem>> TYPE_TO_SYSTEM_MAPINGS = new Dictionary<Type[], List<EntitySystem>>(); 
+        private static readonly List<EntitySystem> SYSTEMS = new List<EntitySystem>(); 
+
+        public static void Register(EntitySystem system, Type[] types)
+        {
+            SYSTEMS.Add(system);
+            if(!TYPE_TO_SYSTEM_MAPINGS.ContainsKey(types))
+                TYPE_TO_SYSTEM_MAPINGS[types] = new List<EntitySystem>();
+                
+            TYPE_TO_SYSTEM_MAPINGS[types].Add(system);
+        }
+
+        public static void Update(GameTime time)
+        {
+            SYSTEMS.ForEach(sys => sys.Update(time));
+        }
+
+        public static void AddEntity(Entity e)
+        {
+            RetrieveSystemsForEntity(e).ForEach(sys => sys.AddEntity(e));
+        }
+
+        private static List<EntitySystem> RetrieveSystemsForEntity(Entity e)
+        {
+            var types = e.Components.Select(comp => comp.GetType()).ToArray();
+            if (!TYPE_TO_SYSTEM_MAPINGS.ContainsKey(types))
+                throw new ArgumentException("Entity type is not bound to any Systems!", "e");
+            return TYPE_TO_SYSTEM_MAPINGS[types];
+        }
+
+        public static void RemoveEntity(Entity e)
+        {
+            RetrieveSystemsForEntity(e).ForEach(sys => sys.RemoveEntity(e));    
+        }
+    }
+}
