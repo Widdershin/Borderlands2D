a2b0afca58e6c14dc3642a53bcdca93c750cec16
Taylor <ubermouse894@gmail.com>
2012-11-04 20:12:52 +1300

    Added EntitySystem

---
+++ ECS/EntitySystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Borderlands2D.ECS
+{
+    public abstract class EntitySystem
+    {
+        protected List<Type> _managedComponentTypes = new List<Type>();
+        protected List<Entity> _entities = new List<Entity>();
+
+        protected EntitySystem()
+        {
+            SystemsRegistry.Register(this, _managedComponentTypes.ToArray());
+        }
+
+        internal void AddEntity(Entity e)
+        {
+            var types = e.ComponentTypes;
+            foreach (var type in _managedComponentTypes.Where(type => !types.Contains(type)))
+                throw new ArgumentException("Entity doesn't contain required Component " + type, "e");
+            _entities.Add(e);
+        }
+
+        internal void RemoveEntity(Entity e)
+        {
+            _entities.Remove(e);
+        }
+
+        public abstract void Update(GameTime time);
+    }
+}
