ed85fcd5d84fdfeb9a0b0b46a75bca8238b89ad3
Taylor <ubermouse894@gmail.com>
2012-11-04 20:13:03 +1300

    Added Entity

---
+++ ECS/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Borderlands2D.ECS
+{
+    public class Entity
+    {
+        public List<Component> Components { get; private set; }
+        public List<Type> ComponentTypes { get { return Components.Select(comp => comp.GetType()).ToList(); } } 
+
+        public Entity()
+        {
+            Components = new List<Component>();
+        }
+
+        public Component GetComponent<T>()
+        {
+            var type = typeof (T);
+            return Components.FirstOrDefault(comp => comp.GetType() == type);
+        }
+    }
+}
