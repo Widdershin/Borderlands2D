8f756884e25bc61b56a4592db0df4187e9a314a0
Taylor <ubermouse894@gmail.com>
2012-11-04 20:51:23 +1300

    Added Position, Sprite and Velocity Components

---
+++ ECS/Components/Velocity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Borderlands2D.ECS.Components
+{
+    class Velocity : Component
+    {
+        public Vector2 Vector { get; set; }
+
+        public Velocity(Vector2 vector)
+        {
+            Vector = vector;
+        }
+
+        public Velocity()
+        {
+            Vector = new Vector2(0, 0);
+        }
+    }
+}
