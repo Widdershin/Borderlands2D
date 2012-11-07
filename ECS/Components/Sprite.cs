d770321c3dada0008c34f35a2e39eb74ddb08221
Taylor <ubermouse894@gmail.com>
2012-11-08 00:12:32 +1300

    Updated Sprite with SpriteOffset Vector

--- ECS/Components/Sprite.cs
+++ ECS/Components/Sprite.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Borderlands2D.ECS.Components
@@ -9,15 +10,28 @@ namespace Borderlands2D.ECS.Components
     class Sprite : IComponent
     {
         public Texture2D Texture { get; set; }
+        public Vector2 Offset;
 
-        public Sprite(Texture2D texture)
+        public Sprite(Texture2D texture, Vector2 offset)
         {
             Texture = texture;
+            Offset = offset;
         }
 
-        public Sprite(string texturePath)
+        public Sprite(Texture2D texture) : this(texture, new Vector2(0, 0))
+        {
+            
+        }
+
+        public Sprite(string texturePath, Vector2 offset)
         {
             Texture = TextureManager.Get(texturePath);
+            Offset = offset;
+        }
+
+        public Sprite(string texturePath) : this(texturePath, new Vector2(0, 0))
+        {
+            
         }
     }
 }
